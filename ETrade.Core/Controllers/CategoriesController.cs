607106a234f9466e73d245cc4b7a25073e010028
Damlaa752 <gultekindamla7@gmail.com>
2023-05-24 14:59:40 +0300

    Web API Eklendi.

---
+++ ETrade.Core/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using ETrade.Entity.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ETrade.Core.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseBody = await httpClient.GetAsync("http://localhost:14694/api/Categories");
+            var jsonString = await responseBody.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Category>>(jsonString);
+            return View(values);
+        }
+    }
+}
